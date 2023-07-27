496fc1af47461f1b2cf52a8c455fb6e908ec9028
Hussein039 <84193192+Hussein039@users.noreply.github.com>
2023-07-27 11:31:35 -0400

    project1 added

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import styles from './css-components/projects.module.css'
+import weatherImage from "../images/weather-image.png"
 
 const Projects = () => {
     
@@ -9,10 +10,10 @@ const Projects = () => {
                 <div className={styles.cards}>
                     <div className={styles.card}>
                         <div className={styles.cardImg}>
-                        <img src='../images/proj.jpg' alt='img'></img>
+                        <img src={weatherImage} alt='weatherImage'></img>
                         </div>
-                        <h3>My Project</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <h3>Weather App</h3>
+                        <p><a href="https://myreactjsweather.netlify.app/" target="__blank">Visit Site</a></p>
                     </div>
                     <div className={styles.card}>
                         <div className={styles.cardImg}>
