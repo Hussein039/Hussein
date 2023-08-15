d2655cea9090e5457e2f7b5c2d06a1b727726414
Hussein039 <84193192+Hussein039@users.noreply.github.com>
2023-08-15 13:54:38 -0400

    Buttom

--- src/components/projects.js
+++ src/components/projects.js
@@ -13,28 +13,28 @@ const Projects = () => {
                         <img src={weatherImage} alt='weatherImage'></img>
                         </div>
                         <h3>Weather App</h3>
-                        <p><a href="https://myreactjsweather.netlify.app/" target="__blank">Visit Site</a></p>
+                        <p><a href="https://myreactjsweather.netlify.app/" target="__blank" className={styles.proj_link}>Visit Site</a></p>
                     </div>
                     <div className={styles.card}>
                         <div className={styles.cardImg}>
-                            <img src='../images/proj.jpg' alt='img'></img>
+                        <img src={weatherImage} alt='weatherImage'></img>
                         </div>
-                        <h3>My Project</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <h3>Weather App</h3>
+                        <p><a href="https://myreactjsweather.netlify.app/" target="__blank" className={styles.proj_link}>Visit Site</a></p>
                     </div>
                     <div className={styles.card}>
                         <div className={styles.cardImg}>
-                        <img src='../images/proj.jpg' alt='img'></img>
+                        <img src={weatherImage} alt='weatherImage'></img>
                         </div>
-                        <h3>My Project</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <h3>Weather App</h3>
+                        <p><a href="https://myreactjsweather.netlify.app/" target="__blank" className={styles.proj_link}>Visit Site</a></p>
                     </div>
                     <div className={styles.card}>
                         <div className={styles.cardImg}>
-                        <img src='../images/proj.jpg' alt='img'></img>
+                        <img src={weatherImage} alt='weatherImage'></img>
                         </div>
-                        <h3>My Project</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <h3>Weather App</h3>
+                        <p><a href="https://myreactjsweather.netlify.app/" target="__blank" className={styles.proj_link}>Visit Site</a></p>
                     </div>
                 </div>
             </div>
